d2397f3c0d8b344f5f2445d1a776f284f118e2d9
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-05-01 11:26:34 -0900

    revisando2

--- model/Punto.java
+++ model/Punto.java
@@ -13,8 +13,15 @@ public class Punto extends Point{
         super(x, y);
     }
 
-    public void rotate (double grados) {
-        
+    public void rotate (double grados,int xPuntoRotar,int yPuntoRotar) {
+        double seno = Math.sin(Math.toRadians(grados));
+        double coseno = Math.cos(Math.toRadians(grados));
+        int auxX = this.x;
+        int auxY = this.y;
+        double xvar = (xPuntoRotar + (auxX - xPuntoRotar)*coseno - (auxY - yPuntoRotar)*seno);
+        double yvar = (yPuntoRotar + (auxX - xPuntoRotar)*seno - (auxY - yPuntoRotar)*coseno);
+        x = (int)xvar;
+        y = (int)yvar;
     }
     
     public void traslate (int dx, int dy) {
